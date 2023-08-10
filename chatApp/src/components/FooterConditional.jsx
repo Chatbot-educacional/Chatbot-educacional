74dbca8a66b9c0dd9ecb14a78f192106f5b1813e
Balda <mateusbalda89@gmail.com>
2023-08-10 01:49:28 -0300

    Fix: diretório do footer

--- chatApp/src/components/FooterConditional.jsx
+++ chatApp/src/components/FooterConditional.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import Footer from './Footer';
+import Footer from './Footer/Footer';
 
 function FooterConditional() {
   const location = useLocation();
