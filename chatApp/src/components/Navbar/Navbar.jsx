9cd0c53897712d6a0150bdddd308f080f35285c4
jauzer <jauzerv@outlook.com>
2023-08-15 11:58:23 -0300

    feat + refactor: responsividade da tela home e criação de uma nav bar responsiva

--- chatApp/src/components/Navbar/Navbar.jsx
+++ chatApp/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import { NavLink } from "react-router-dom"
-
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
@@ -8,28 +7,35 @@ const Navbar = () => {
         <NavLink to="/" className={styles.brand}>
             Chat <span>Bot</span>
         </NavLink>
-        <ul className={styles.links_list}>
-            <li>
-                <NavLink to="/" className={({isActive}) => (isActive ? styles.active : '')}>Home</NavLink>
-            </li>
-            <li>
-                <NavLink to="/chat" className={({isActive}) => (isActive ? styles.active : '')}>Chat</NavLink>
-            </li>
-            <li>
-                <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : '')}>Sobre</NavLink>
-            </li>
-            <li>
-                <NavLink to="/contact" className={({isActive}) => (isActive ? styles.active : '')}>Contato</NavLink>
-            </li>
-        </ul>
-        <ul className={styles.links_list_right}>
-            <li>
-                <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : '')}>Entrar</NavLink>
-            </li>
-            <li>
-                <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : '')}>Cadastrar</NavLink>
-            </li>
-        </ul>
+        <div className={styles.menu_toggle_on}>
+            <div className={styles.menu_toggle}>
+                <div className={styles.one}></div>
+                <div className={styles.two}></div>
+                <div className={styles.three}></div>
+            </div>
+            <ul className={styles.links_list}>
+                <li>
+                    <NavLink to="/" className={({isActive}) => (isActive ? styles.active : '')}>Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/chat" className={({isActive}) => (isActive ? styles.active : '')}>Chat</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : '')}>Sobre</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/contact" className={({isActive}) => (isActive ? styles.active : '')}>Contato</NavLink>
+                </li>
+            </ul>
+            <ul className={styles.links_list_right}>
+                <li>
+                    <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : '')}>Entrar</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : '')}>Cadastrar</NavLink>
+                </li>
+            </ul>
+        </div>
     </nav>
   )
 }
