adcf11fdf797e714fc71e8e4813c63306f775516
Balda <mateusbalda89@gmail.com>
2023-08-16 16:51:22 -0300

    feat: login do usuário e proteção de rotas

--- chatApp/src/components/Navbar/Navbar.jsx
+++ chatApp/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,15 @@
 import { NavLink } from "react-router-dom"
 
+import { useAuthentication } from "../../hooks/useAuthentication";
+
+import { useAuthValue } from "../../context/AuthContext";
+
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
+  const { user } = useAuthValue();
+  const { logout } = useAuthentication();
+
   return (
     <nav className={styles.navbar}>
         <NavLink to="/" className={styles.brand}>
@@ -12,9 +19,13 @@ const Navbar = () => {
             <li>
                 <NavLink to="/" className={({isActive}) => (isActive ? styles.active : '')}>Home</NavLink>
             </li>
-            <li>
-                <NavLink to="/chat" className={({isActive}) => (isActive ? styles.active : '')}>Chat</NavLink>
-            </li>
+            {user && (
+                <>
+                    <li>
+                    <NavLink to="/chat" className={({isActive}) => (isActive ? styles.active : '')}>Chat</NavLink>
+                    </li>
+                </>
+            )}
             <li>
                 <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : '')}>Sobre</NavLink>
             </li>
@@ -23,12 +34,21 @@ const Navbar = () => {
             </li>
         </ul>
         <ul className={styles.links_list_right}>
-            <li>
-                <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : '')}>Entrar</NavLink>
-            </li>
-            <li>
-                <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : '')}>Cadastrar</NavLink>
-            </li>
+        {!user && (
+            <>
+                <li>
+                    <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : '')}>Entrar</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : '')}>Cadastrar</NavLink>
+                </li>
+            </>
+        )}
+            {user && (
+                <li>
+                    <NavLink onClick={logout} className={({isActive}) => (isActive ? styles.active : '')}>Sair</NavLink>
+                </li>
+            )}
         </ul>
     </nav>
   )
--- chatApp/src/hooks/useAuthentication.js
+++ chatApp/src/hooks/useAuthentication.js
@@ -26,6 +26,7 @@ export const useAuthentication = () => {
         }
     }
 
+    // register
     const createUser = async (data) => {
         checkIfIsCancelled();
         setCancelled(true);
@@ -63,6 +64,37 @@ export const useAuthentication = () => {
         }
     };
 
+    // logout - sign out
+    const logout = () => {
+        checkIfIsCancelled();
+
+        signOut(auth);
+    };
+
+    // login - sign in
+    const login = async (data) => {
+        checkIfIsCancelled();
+        setLoading(true);
+        setError(false);
+
+        try {
+            await signInWithEmailAndPassword(auth, data.email, data.password)
+            setLoading(false);
+        } catch (error) {
+            let systemErrorMessage
+
+            if (error.message.includes("user-not-found")) {
+                systemErrorMessage = "Usuário não encontrado"
+            } else if (error.message.includes("wrong-password")) {
+                systemErrorMessage = "Senha incorreta"
+            } else {
+                systemErrorMessage = "Ocorreu um erro, por favor tente novamente mais tarde"
+            }
+            setError(systemErrorMessage);
+            setLoading(false);
+        }
+    }
+
     useEffect(() => {
         return () => setCancelled(true);
     }, []);
@@ -72,5 +104,7 @@ export const useAuthentication = () => {
         createUser,
         error,
         loading,
+        logout,
+        login,
     }
 }
