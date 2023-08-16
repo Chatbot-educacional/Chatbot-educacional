04c530a8aca0d2938db82418bd0c388e1e9146e7
jauzer <jauzerv@outlook.com>
2023-08-15 21:11:32 -0300

    feat + refactor: avanços na responsividade da tela home, e alterações na Navbar responsiva

--- chatApp/src/components/Navbar/Navbar.jsx
+++ chatApp/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,20 @@
 import { NavLink } from "react-router-dom"
 import styles from './Navbar.module.css';
+import { Component } from "react";
 
-const Navbar = () => {
-  return (
+class Navbar extends Component{
+
+    state ={clicked : false}; /*verifica se clicou no menu do mobile*/
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
+    render() {
+    return (
     <nav className={styles.navbar}>
         <NavLink to="/" className={styles.brand}>
             Chat <span>Bot</span>
         </NavLink>
-        <div className={styles.menu_toggle_on}>
-            <div className={styles.menu_toggle}>
-                <div className={styles.one}></div>
-                <div className={styles.two}></div>
-                <div className={styles.three}></div>
-            </div>
             <ul className={styles.links_list}>
                 <li>
                     <NavLink to="/" className={({isActive}) => (isActive ? styles.active : '')}>Home</NavLink>
@@ -35,9 +37,12 @@ const Navbar = () => {
                     <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : '')}>Cadastrar</NavLink>
                 </li>
             </ul>
-        </div>
+        {/* <div className={styles.mobile} onClick={this.handleClick}>
+            <i id="bar" className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        </div> */}
+
     </nav>
   )
 }
-
+}
 export default Navbar
