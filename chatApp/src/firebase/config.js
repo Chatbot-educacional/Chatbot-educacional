047aa2d5e57ad51be3c1bfe2933d6037f57c015b
Balda <mateusbalda89@gmail.com>
2023-11-02 22:25:33 -0300

    refactor: chatbot options dialogues

--- chatApp/src/firebase/config.js
+++ chatApp/src/firebase/config.js
@@ -1,20 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
 
 export const firebaseConfig = {
   apiKey: "AIzaSyAT6CATosrnNmy9tpTB8GEenPcHcJBuA8k",
   authDomain: "chatboteduca.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
   projectId: "chatboteduca",
   storageBucket: "chatboteduca.appspot.com",
   messagingSenderId: "688718917755",
-  appId: "1:688718917755:web:420452363a0d56175232de"
+  appId: "1:688718917755:web:420452363a0d56175232de",
+  measurementId: "G-9VP01NDSXJ"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const analytics = getAnalytics(app);
 const provider = new GoogleAuthProvider();
 
-export {auth,provider};
+export {auth, provider, analytics};
 
 export const db = getFirestore(app);
